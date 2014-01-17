44b71df8a217b9e719f748d2d6243da18970cf00
butschster <butschster@gmail.com>
2014-01-17 11:33:19 +0400

    init

---
+++ init.php
@@ -0,0 +1,6 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+Plugin::factory('page_not_found', array(
+	'title' => 'Page not found',
+	'description' => 'Provides Page not found type.',
+))->register();
